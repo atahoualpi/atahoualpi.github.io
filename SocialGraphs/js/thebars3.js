852e63341641867856e12acce7643176f548c7be
atahoualpi <s151296@student.dtu.dk>
2016-11-29 23:00:22 +0100

    Add files via upload

--- SocialGraphs/js/thebars3.js
+++ SocialGraphs/js/thebars3.js
@@ -1,79 +1,214 @@
 var margin = {top: 20, right: 30, bottom: 40, left: 100},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-var padding2 = 30;
-
-var x = d3.scale.linear()
-    .range([0, width]);
-
-var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], 0.1);
-
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom");
-
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left")
-//     .tickSize(0)
-//     .tickPadding(6);
-
-// var svg = d3.select("body").append("svg")
-var chartHor = d3.select("#chartHo")
-
+var chartHorBackground = d3.select("#chartHo")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data/data.tsv", type, function(error, data) {
-  x.domain(d3.extent(data, function(d) { return d.value; })).nice();
-  y.domain(data.map(function(d) { return d.name; }));
+var chartHorFill = d3.select("#chartHo")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var yScale1 = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d,i){return i*100})])
-                    .range([height-padding2, padding2]);
+// var tooltip;
+// var div;
 
-  chartHor.selectAll("rect")
-      .data(data)
-      .enter().append("rect")
-      // .attr("class", function(d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive"); })
-      .attr("x", function(d) { return x(Math.min(0, d.value)); })
-      .attr("y", function(d) { return y(d.name); })
-      .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
-      .attr("height", y.rangeBand())
-      // .attr("fill", function(d){ return "rgb(0,"+(d.value*1000)+",0)"; });
-           .attr("fill", "red");
+
+//
+// Sentimental Analysis
+//
+chartHorFill              
+    .append("text")
+    .text("Sentimental")
+    .attr("x", -97)
+    .attr("y", 30)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue");  
+  chartHorFill              
+    .append("text")
+    .text("Analysis")
+    .attr("x", -85)
+    .attr("y", 50)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue"); 
+
+  chartHorBackground.append("rect")
+    .attr("x", 30)
+    .attr("y", 10)
+    .attr("width", 500)
+    .attr("height", 50)
+    .attr("opacity", 0.6)
+    .attr("fill", "rgb(255,195,255)"); 
+
+//
+// Degree
+//
+chartHorFill              
+    .append("text")
+    .text("Amount of")
+    .attr("x", -90)
+    .attr("y", 130)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue");  
+  chartHorFill              
+    .append("text")
+    .text("Connections")
+    .attr("x", -97)
+    .attr("y", 150)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue"); 
+
+  chartHorBackground.append("rect")
+    .attr("x", 30)
+    .attr("y", 110)
+    .attr("width", 500)
+    .attr("height", 50)
+    .attr("opacity", 0.6)
+    .attr("fill", "rgb(255,195,255)"); 
+
+function barData(element){
+
+  d3.selectAll("#chartHo > *").remove();
+  console.log(element)
+
+  chartHorBackground = d3.select("#chartHo")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  chartHorFill = d3.select("#chartHo")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+  var div = d3.select("#chartHo").append("div")   
+      .attr("class", "tooltip")               
+      .style("opacity", 0);
+
+  //
+  // Sentimental Analysis
+  //    
+  chartHorFill
+  .append("rect")
  
+    .attr("x", 30)
+    .attr("y", 10)
+    .attr("width", element.sentiment*10)
+    .attr("height", 50)
+    .attr("fill", "purple");
+  
+  chartHorBackground.append("rect")
+    .attr("x", 30)
+    .attr("y", 10)
+    .attr("width", 500)
+    .attr("height", 50)
+    .attr("opacity", 0.6)
+    .attr("fill", "rgb(255,195,255)")
 
- chartHor.selectAll("text")
-                    .data(data)
-                    .enter()
-                    .append("text")
-                    .text(function(d){return d.value})
-                    .attr("x", 0)
-                    .attr("y", function(d,i){ return 10+i*80})
-                    // .attr("y", function(d,i){
-                    //     return yScale1(d.value*1000)})
-                    .attr("font-family", 'FontAwesome')
-                    .attr("font-size", "11px")
-                    .attr("fill", function(d, i){
-                            return "rgb("+(178-i*10)+","+(128+i)+","+(78+i*10)+")"
-                    }); 
-  // chartHor.append("g")
-  //      .attr("class", "x axis")
-  //     .attr("transform", "translate(0," + height + ")")
-  //      .call(xAxis);
+  
 
-  // chartHor.append("g")
-  //      .attr("class", "y axis")
-  //     .attr("transform", "translate(" + x(0) + ",0)")
-  //     .call(yAxis);
-});
 
-function type(d) {
-  d.value = +d.value;
-  return d;
+    
+
+  chartHorFill              
+    .append("text")
+    .text("Sentimental")
+    .attr("x", -97)
+    .attr("y", 30)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue");  
+  chartHorFill              
+    .append("text")
+    .text("Analysis")
+    .attr("x", -85)
+    .attr("y", 50)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px");
+    
+
+  chartHorBackground              
+    .append("text")
+    .text(element.sentiment.toFixed(2))
+    .attr("x", 450)
+    .attr("y", 50)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue")
+    .attr("fill","blue")
+    // .on("click", 
+      // div.transition()
+      //     .duration(200)
+      //     .style("opacity", .9)
+      // div
+// .attr("x", d3.event.pageX)
+// .attr("y", d3.event.pageY)
+    // div.text("Based on tracklist")
+    //         .style("opacity",0.9)
+    //          .style("left", (d3.event.pageX) + "px")     
+    //         .style("top", (d3.event.pageY) + "px")
+    //         .style("opacity", .9));
+                  // console.log(div.style("top"))
+                  // console.log(div.attr("x"))
+                  // console.log(div.attr("y"))
+
+    // .on("mouseoout", function(){
+    //   div.transition()
+    //       .duration(500)
+    //       .style("opacity", 0)});
+
+  //
+  // Degree
+  //    
+  chartHorFill.append("rect")
+    .attr("x", 30)
+    .attr("y", 110)
+    .attr("width", element.degree*10)
+    .attr("height", 50)
+    .attr("fill", "purple");
+  
+  chartHorBackground.append("rect")
+    .attr("x", 30)
+    .attr("y", 110)
+    .attr("width", 500)
+    .attr("height", 50)
+    .attr("opacity", 0.6)
+    .attr("fill", "rgb(255,195,255)");   
+
+  chartHorFill              
+    .append("text")
+    .text("Amount of")
+    .attr("x", -90)
+    .attr("y", 130)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue");  
+  chartHorFill              
+    .append("text")
+    .text("Connections")
+    .attr("x", -97)
+    .attr("y", 150)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue"); 
+
+  chartHorBackground              
+    .append("text")
+    .text(element.degree.toFixed(2))
+    .attr("x", 450)
+    .attr("y", 150)
+    .attr("font-family", 'FontAwesome')
+    .attr("font-size", "20px")
+    .attr("fill","blue");  
 }
+
